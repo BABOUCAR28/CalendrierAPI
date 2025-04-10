c2459cd5309f614e4e2dc348dfc66ef4951fe10e
Koumba Thiongane <koumbathiongane201@gmail.com>
2025-04-10 21:45:49 +0200

    commit one

---
+++ src/main/java/sn/uasz/CalendriersAPI/CalendriersApiApplication.java
@@ -0,0 +1,13 @@
+package sn.uasz.CalendriersAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendriersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendriersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sn/uasz/CalendriersAPI/CalendriersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uasz.CalendriersAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendriersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
